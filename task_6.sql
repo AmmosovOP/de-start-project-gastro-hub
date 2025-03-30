0fb0cb01d5f33a2859be92c86cafa039f3ee0842
AmmosovOP <112493590+AmmosovOP@users.noreply.github.com>
2025-03-30 16:07:35 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,21 @@
 /*добавьте сюда запросы для решения задания 6*/
+BEGIN;
+
+WITH cafes_with_cappuccino AS (
+    SELECT r.restaurant_uuid
+    FROM cafe.restaurants r
+    WHERE r.menu->'Напитки' ? 'Капучино'
+    FOR UPDATE OF r  
+)
+
+UPDATE cafe.restaurants r
+SET menu = jsonb_set(
+    menu,
+    '{Напитки,Капучино}',
+    to_jsonb(ROUND((menu->'Напитки'->>'Капучино')::numeric * 1.2, 2)::text::jsonb
+)
+WHERE r.restaurant_uuid IN (
+    SELECT restaurant_uuid FROM cafes_with_cappuccino
+);
+
+COMMIT;
