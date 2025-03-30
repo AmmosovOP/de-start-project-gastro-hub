42013905a20a7f57f2653f984bf9f493bc03bdcf
AmmosovOP <112493590+AmmosovOP@users.noreply.github.com>
2025-03-30 16:06:54 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,21 @@
 /*добавьте сюда запрос для решения задания 3*/
+CREATE VIEW cafe.top_restaurants_by_manager_changes AS
+WITH manager_changes AS (
+    SELECT
+        r.cafe_name AS "Название заведения",
+        COUNT(DISTINCT w.manager_uuid) AS manager_count
+    FROM
+        cafe.restaurant_manager_work_dates w
+    JOIN
+        cafe.restaurants r ON w.restaurant_uuid = r.restaurant_uuid
+    GROUP BY
+        r.cafe_name
+)
+SELECT
+    "Название заведения",
+    manager_count - 1 AS "Сколько раз менялся менеджер"
+FROM
+    manager_changes
+ORDER BY
+    "Сколько раз менялся менеджер" DESC
+LIMIT 3; 
