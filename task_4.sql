b64dcff04cd44f5434082ff650f65547b446c1c7
AmmosovOP <112493590+AmmosovOP@users.noreply.github.com>
2025-03-30 16:07:08 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,31 @@
 /*добавьте сюда запрос для решения задания 4*/
+CREATE VIEW cafe.pizzerias_with_most_pizzas AS
+WITH pizza_counts AS (
+    SELECT
+        r.cafe_name AS "Название заведения",
+        COUNT(*) AS "Количество пицц в меню"
+    FROM
+        cafe.restaurants r,
+        jsonb_each_text(r.menu->'Пицца') AS pizza_items
+    WHERE
+        r.type = 'pizzeria'
+    GROUP BY
+        r.cafe_name
+),
+ranked_pizzerias AS (
+    SELECT
+        "Название заведения",
+        "Количество пицц в меню",
+        DENSE_RANK() OVER (ORDER BY "Количество пицц в меню" DESC) AS rank
+    FROM
+        pizza_counts
+)
+SELECT
+    "Название заведения",
+    "Количество пицц в меню"
+FROM
+    ranked_pizzerias
+WHERE
+    rank = 1
+ORDER BY
+    "Название заведения";
