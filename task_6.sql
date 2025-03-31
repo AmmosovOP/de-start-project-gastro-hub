bf7ea2e078a0206731c815f14a644ad8d077b0ec
AmmosovOP <112493590+AmmosovOP@users.noreply.github.com>
2025-03-31 18:16:42 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -4,7 +4,7 @@ BEGIN;
 WITH cafes_with_cappuccino AS (
     SELECT r.restaurant_uuid
     FROM cafe.restaurants r
-    WHERE r.menu->'Напитки' ? 'Капучино'
+    WHERE r.menu->'Кофе' ? 'Капучино'
     FOR UPDATE OF r  
 )
 
@@ -12,7 +12,7 @@ UPDATE cafe.restaurants r
 SET menu = jsonb_set(
     menu,
     '{Напитки,Капучино}',
-    to_jsonb(ROUND((menu->'Напитки'->>'Капучино')::numeric * 1.2, 2)::text::jsonb
+    to_jsonb(ROUND((menu->'Кофе'->>'Капучино')::numeric * 1.2, 2))
 )
 WHERE r.restaurant_uuid IN (
     SELECT restaurant_uuid FROM cafes_with_cappuccino
